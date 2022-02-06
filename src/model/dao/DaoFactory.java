2844d6e959ca6f29ae3619b28943136fc37a8ac2
Erik Paiva <erikbppaiva@gmail.com>
2022-02-06 14:34:13 -0300

    Reusing instantiation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -10,15 +10,3 @@ public class DaoFactory {
 	}
 
 }
-//package model.dao;
-//
-//import db.DB;
-//import model.dao.impl.SellerDaoJDBC;
-//
-//public class DaoFactory {
-//
-//	public static SellerDao createSellerDao() {
-//
-//		return new SellerDaoJDBC(DB.getConnection());
-//	}
-//}
